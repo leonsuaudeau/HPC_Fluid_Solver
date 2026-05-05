ae636a6e4b2b627adf66f984cd8f4473955e0e13
leonsuaudeau <friedrichsuaudeau@gmail.com>
2026-05-05 18:24:18 +0200

    Reached milestone 1, streaming function and matplotlib animation working

--- executables/main.cpp
+++ executables/main.cpp
@@ -1,30 +1,157 @@
-#include "hello.h"
-#include <iostream>
-#include <filesystem>
-#include <mpi.h>
 #include <Kokkos_Core.hpp>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <mpi.h>
 
+KOKKOS_INLINE_FUNCTION
+int wrap(const int i, const int m) {
+    return (i % m + m) % m;
+}
+
+KOKKOS_INLINE_FUNCTION
+void update_density(
+    const Kokkos::View<float**> &rho,
+    const Kokkos::View<float***> &f,
+    const int x, const int y)
+{
+    float sum = 0.0f;
+    for (int i = 0; i < 9; i++) {
+        sum += f(x, y, i);
+    }
+    rho(x, y) = sum;
+}
+
+KOKKOS_INLINE_FUNCTION
+void update_velocity(
+    const Kokkos::View<float**> &v_x,
+    const Kokkos::View<float**> &v_y,
+    const Kokkos::View<int*> &c_x,
+    const Kokkos::View<int*> &c_y,
+    const Kokkos::View<float**> &rho,
+    const Kokkos::View<float***> &f,
+    const int x, const int y)
+{
+    float sum_x = 0.0f;
+    float sum_y = 0.0f;
+    for (int i = 0; i < 9; i++) {
+        sum_x += f(x, y, i) * c_x(i);
+        sum_y += f(x, y, i) * c_y(i);
+    }
+    v_x(x, y) = sum_x / rho(x, y);
+    v_y(x, y) = sum_y / rho(x, y);
+}
+
+KOKKOS_INLINE_FUNCTION
+void streaming(
+    const Kokkos::View<float***> &f,
+    const Kokkos::View<float***> &f_new,
+    const Kokkos::View<int*> &c_x,
+    const Kokkos::View<int*> &c_y,
+    const int x, const int y, const int grid_width, const int grid_height)
+{
+    for (int i = 0; i < 9; i++) {
+        const int x_old = wrap(x - c_x(i), grid_width);
+        const int y_old = wrap(y - c_y(i), grid_height);
+        f_new(x, y, i) = f(x_old, y_old, i);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    int rank = 0, size = 1;
-
     MPI_Init(&argc, &argv);
     Kokkos::initialize(argc, argv);
+    {
+        int grid_width, grid_height;
+        grid_width = grid_height = 32;
+        int timestep = 0;
+        int max_steps = 100;
+        int draw_steps = 1;
 
-    // Retrieve process infos
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+        Kokkos::View<int*> c_x("c_x", 9);
+        Kokkos::View<int*> c_y("c_y", 9);
+        auto c_x_host = Kokkos::create_mirror_view(c_x);
+        auto c_y_host = Kokkos::create_mirror_view(c_y);
 
-    std::cout << "Hello I am rank " << rank << " of " << size << "\n";
+        // D2Q9 scheme initialization
+        c_x_host(0) = 0.0f; c_y_host(0) = 0.0f;
+        c_x_host(1) = 0.0f; c_y_host(1) = 1.0f;
+        c_x_host(2) = 1.0f; c_y_host(2) = 0.0f;
+        c_x_host(3) = 0.0f; c_y_host(3) = -1.0f;
+        c_x_host(4) = -1.0f; c_y_host(4) = 0.0f;
+        c_x_host(5) = 1.0f; c_y_host(5) = 1.0f;
+        c_x_host(6) = 1.0f; c_y_host(6) = -1.0f;
+        c_x_host(7) = -1.0f; c_y_host(7) = -1.0f;
+        c_x_host(8) = -1.0f; c_y_host(8) = 1.0f;
 
-    if (rank == 0)
-      hello_world();
+        Kokkos::deep_copy(c_x, c_x_host);
+        Kokkos::deep_copy(c_y, c_y_host);
+
+        Kokkos::View<float**> rho("rho", grid_width, grid_height);
+        Kokkos::View<float**> v_x("v_x", grid_width, grid_height);
+        Kokkos::View<float**> v_y("v_y", grid_width, grid_height);
+        Kokkos::View<float***> f("f", grid_width, grid_height, 9);
+        Kokkos::View<float***> f_new("f_new", grid_width, grid_height, 9);
+
+        auto rho_host = Kokkos::create_mirror_view(rho);
+        auto v_x_host = Kokkos::create_mirror_view(v_x);
+        auto v_y_host = Kokkos::create_mirror_view(v_y);
+        auto f_host = Kokkos::create_mirror_view(f);
+
+        // Grid initializations
+        for (int x = 0; x < grid_width; x++) {
+            for (int y = 0; y < grid_height; y++) {
+                rho_host(x, y) = 1.0f;
+                v_x_host(x, y) = 0.0f; // small flow for testing
+                v_y_host(x, y) = 0.0f;
+                for (int i = 0; i < 9; i++) {
+                    if (x == 31 && i == 4) {
+                        f_host(x, y, i) = 1.0f;
+                        continue;
+                    }
+                    f_host(x, y, i) = 0.01f; // static_cast<float>(random());
+                }
+            }
+        }
+
+        Kokkos::deep_copy(rho, rho_host);
+        Kokkos::deep_copy(v_x, v_x_host);
+        Kokkos::deep_copy(v_y, v_y_host);
+        Kokkos::deep_copy(f, f_host);
 
 
-    auto input_path = "./simulation_test_input.txt";
+        while (timestep < max_steps) {
+            // Simulation step
+            Kokkos::parallel_for("update_density",
+                Kokkos::MDRangePolicy({0,0}, {grid_width, grid_height}),
+                KOKKOS_LAMBDA(const int x, const int y) {
+                    update_density(rho, f, x, y);
+                    update_velocity(v_x, v_y, c_x, c_y, rho, f, x, y);
+                    streaming(f, f_new, c_x, c_y, x, y, grid_width, grid_height);
+                }
+            );
+            Kokkos::deep_copy(f, f_new);
 
-    if (not std::filesystem::exists(input_path))
-      std::cerr << "warning: could not find input file " << input_path << "\n";
+            if (timestep % draw_steps == 0) {
+                Kokkos::deep_copy(v_x_host, v_x);
+                Kokkos::deep_copy(v_y_host, v_y);
+
+                std::ostringstream filename;
+                filename << "../../outputs/v_x_" << std::setw(5) << std::setfill('0') << std::to_string(timestep / draw_steps) << ".bin";
+                std::ofstream file(filename.str(), std::ios::binary);
+                file.write(reinterpret_cast<char*>(v_x_host.data()), grid_width * grid_height * sizeof(float));
+                file.close();
+
+                std::ostringstream filename2;
+                filename2 << "../../outputs/v_y_" << std::setw(5) << std::setfill('0') << std::to_string(timestep / draw_steps) << ".bin";
+                std::ofstream file2(filename2.str(), std::ios::binary);
+                file2.write(reinterpret_cast<char*>(v_y_host.data()), grid_width * grid_height * sizeof(float));
+                file2.close();
+
+                std::cout << "Timestep " << timestep << "/" << max_steps << std::endl;
+            }
+            timestep++;
+        }
+    }
 
     Kokkos::finalize();
     MPI_Finalize();
---
+++ executables/plot.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import glob
+from enum import Enum
+
+def plot_single():
+    v_x =  np.fromfile("outputs/v_x.bin", dtype=np.float32).reshape((width, height))
+    v_y =  np.fromfile("outputs/v_y.bin", dtype=np.float32).reshape((width, height))
+
+    plt.imshow(v_x)
+    plt.colorbar()
+    plt.show()
+
+def animate_arrows(skip, scale):
+    X, Y = np.meshgrid(
+        np.arange(0, height, skip),
+        np.arange(0, width, skip)
+    )
+
+    fig, ax = plt.subplots()
+
+    q = ax.quiver(
+        X[::skip, ::skip],
+        Y[::skip, ::skip],
+        frames_y[0][::skip, ::skip],
+        frames_x[0][::skip, ::skip],
+        scale=scale,
+    )
+
+    def update_frame(frame_idx):
+        # flipping to make the arrows point in the right direction
+        vx = frames_y[frame_idx]
+        vy = frames_x[frame_idx]
+
+        q.set_UVC(
+            vx[::skip, ::skip],
+            vy[::skip, ::skip]
+        )
+        return [q]
+
+    anim = animation.FuncAnimation(fig, update_frame, frames=len(frames_x), interval=100, blit=True)
+    plt.show()
+    return anim
+
+def animate_magnitude():
+    fig, ax = plt.subplots()
+    mag_0 = np.sqrt(frames_x[0]**2 + frames_y[0]**2)
+    im = ax.imshow(mag_0, origin='lower', cmap='viridis')
+    plt.colorbar(im)
+
+    def update_frame(frame_idx):
+        mag = np.sqrt(frames_x[frame_idx]**2 + frames_y[frame_idx]**2)
+        im.set_array(mag)
+        return [im]
+
+    anim = animation.FuncAnimation(fig, update_frame, frames=len(frames_x), interval=100, blit=True)
+    plt.show()
+    return anim
+
+class AnimationType(Enum):
+    arrows = 0
+    magnitude = 1
+
+if __name__ == "__main__":
+    width, height = 32, 32
+    animation_type = AnimationType.arrows
+    files_x = sorted(glob.glob("outputs/v_x_*.bin"))
+    files_y = sorted(glob.glob("outputs/v_y_*.bin"))
+    frames_x = [np.fromfile(file, dtype=np.float32).reshape((width, height)) for file in files_x]
+    frames_y = [np.fromfile(file, dtype=np.float32).reshape((width, height)) for file in files_y]
+
+    if animation_type == AnimationType.arrows:
+        anim = animate_arrows(1, 50)
+        anim.save(
+            "lbm_velocity.mp4",
+            writer="ffmpeg",
+            fps=30,
+            dpi=200
+        )
+    elif animation_type == AnimationType.magnitude:
+        anim = animate_magnitude()
